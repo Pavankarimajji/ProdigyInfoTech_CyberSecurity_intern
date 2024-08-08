f3027c513a152a196e9820095e825352bc8e4605
Pavankarimajji <162949907+Pavankarimajji@users.noreply.github.com>
2024-08-08 21:56:29 +0530

    Update PasswordComplexityChecker.py

--- Prodigy_CS_03/PasswordComplexityChecker.py
+++ Prodigy_CS_03/PasswordComplexityChecker.py
@@ -1,32 +1,39 @@
 from termcolor import colored
+import pyfiglet
+
 anti_caps = "qwertyuiopasdfghjklzxcvbnm"
 caps = "QWERTYUIOPASDFGHJKLZXCVBNM"
 num = '0123456789'
 special = "!@#$%^&*()_+"
 
+text = "Password Complexity Checker"
+fonts = ["slant"]
+
+for font in fonts:
+    ascii_art = pyfiglet.figlet_format(text, font=font)
+    print(colored(f"Name: {'Pavan'}\n{ascii_art}\n", 'red'))
 password = input("Enter your Password : ")
 
 r = len(password)
-re ,c,ac,n,sp= False,False,False,False,False
+c,ac,n,sp= False,False,False,False
 
-print(r)
 for i in range(0,r):
     if r<8:
         print("more sure password must be 8 or more ")
         break
-    elif password[i] in caps:
+    if password[i] in caps:
         c = True
-    elif password[i] in anti_caps:
+    if password[i] in anti_caps:
         ac = True
-    elif password[i] in num:
+    if password[i] in num:
         n = True
-    elif password[i] in special:
+    if password[i] in special:
         sp = True
 
 re = (c,ac,n,sp)
 if all(re):
     print(colored("your password is secured","green"))
 else:
-    print(colored("oops you need to give secure password","red"))    
+    print("<----"+colored(password,"red")+"----> \noops you need to give secure password\n" +colored("password should have caps,small_letters,special_chars_numbers","red"))    
 
 
