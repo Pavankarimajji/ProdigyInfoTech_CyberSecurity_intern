070279ce1256b7d8c130744e0476b3ef274ee3e0
Pavankarimajji <162949907+Pavankarimajji@users.noreply.github.com>
2024-08-12 13:10:23 +0530

    Update PRODIGY_CS_5.py

--- Prodigy_CS_05/PRODIGY_CS_5.py
+++ Prodigy_CS_05/PRODIGY_CS_5.py
@@ -1,3 +1,11 @@
+'''
+Network Packet Analyzer
+
+Develop a packet sniffer tool that captures and analyzes network packets. 
+Display relevant information such as source and destination IP addresses, protocols, and payload data. 
+Ensure the ethical use of the tool for educational purposes.
+'''
+
 from scapy.all import sniff, IP, TCP, UDP, Raw
 
 def packet_callback(packet):
