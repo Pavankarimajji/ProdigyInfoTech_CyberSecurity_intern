762fb08a144d71af415da9f1f360b01a67dcb8fa
Pavankarimajji <162949907+Pavankarimajji@users.noreply.github.com>
2024-08-12 13:11:21 +0530

    Update PRODIGY_CS_01.py

--- Prodigy_CS_01/PRODIGY_CS_01.py
+++ Prodigy_CS_01/PRODIGY_CS_01.py
@@ -1,6 +1,12 @@
 #CyberSecurity_Intern #task1
 #Prodigy_Info_Tech
 #Pavan_Naga_Durga_Ramesh_Karimajji
+'''
+Implement Caesar Cipher
+
+Create a Python program that can encrypt and decrypt text using the Caesar Cipher algorithm. 
+Allow users to input a message and a shift value to perform encryption and decryption.
+'''
 
 
 
